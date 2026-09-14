a10ceb58eb0a5baa11c82efe1f928d8591e5e630
danbrih <72515313+danbrih@users.noreply.github.com>
2026-09-14 09:56:44 -0300

    creando test combate servicio

---
+++ src/Tests/CombateServiceTests.cs
@@ -0,0 +1,33 @@
+namespace Tests;
+
+using Aplicacion.Dominio;
+using Aplicacion.Servicios;
+using Xunit;
+
+public class CombateServiceTests
+{
+    [Fact]
+    public void GuerreroAplicaFuerzaAdicionalAlAtacar()
+    {
+        var guerrero = new Guerrero { Nombre = "Conan", Ataque = 10, Defensa = 2, Vida = 100 };
+        var mago = new Mago { Nombre = "Gandalf", Ataque = 5, Defensa = 1, Vida = 50 };
+        var service = new CombateService();
+
+        int dano = service.EjecutarTurno(guerrero, mago);
+
+        Assert.Equal(15, dano);
+        Assert.Equal(36, mago.Vida);
+    }
+
+    [Fact]
+    public void MagoConsumeManaAlAtacar()
+    {
+        var mago = new Mago { Nombre = "Gandalf", Ataque = 10, Mana = 10, Vida = 50 };
+        var guerrero = new Guerrero { Nombre = "Conan", Ataque = 10, Defensa = 0, Vida = 100 };
+        var service = new CombateService();
+
+        service.EjecutarTurno(mago, guerrero);
+
+        Assert.Equal(5, mago.Mana);
+    }
+}
