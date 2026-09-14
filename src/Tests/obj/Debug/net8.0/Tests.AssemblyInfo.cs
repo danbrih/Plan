cfc65054cc75ce70b523ce513aa34dc0938d2994
danbrih <72515313+danbrih@users.noreply.github.com>
2026-09-14 10:10:02 -0300

    fix

--- src/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ src/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d02a5bce53b765fdeb72cc9472757c73d49039ed")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aplicacion")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion")]
+[assembly: System.Reflection.AssemblyProductAttribute("Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
